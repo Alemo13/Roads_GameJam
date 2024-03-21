4823445efd66edc1fb1615f7667d817283694a1e
Felipe Castillo <fcp323@gmail.com>
2024-03-21 09:59:40 -0500

    Fixed infinite scroll and tile collider

--- Assets/Felipe/Scripts/ParallaxEffect.cs
+++ Assets/Felipe/Scripts/ParallaxEffect.cs
@@ -5,22 +5,39 @@ using UnityEngine;
 public class ParallaxEffect : MonoBehaviour
 {
     [SerializeField] private float parallaxMultiplier = 0f;
-    
+
     private Transform cameraTransform;
     private Vector3 lastCameraPosition;
-    
+    private float spriteWidth, startPosition;
+
     // Start is called before the first frame update
     void Start()
     {
         cameraTransform = Camera.main.transform;
         lastCameraPosition = cameraTransform.position;
+        spriteWidth = GetComponentInChildren<SpriteRenderer>().bounds.size.x;
+        startPosition = transform.position.x;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
         float deltaX = (cameraTransform.position.x - lastCameraPosition.x) * parallaxMultiplier;
+        float moveAmount = cameraTransform.position.x * (1 - parallaxMultiplier);
         transform.Translate(new Vector3(deltaX, 0, 0));
         lastCameraPosition = cameraTransform.position;
+
+
+        // Infinite Scroll, left and right
+        if (moveAmount > startPosition + spriteWidth)
+        {
+            transform.Translate(new Vector3(spriteWidth, 0, 0));
+            startPosition += spriteWidth;
+        }
+        else if (moveAmount < startPosition - spriteWidth)
+        {
+            transform.Translate(new Vector3(-spriteWidth, 0, 0));
+            startPosition -= spriteWidth;
+        }
     }
 }
