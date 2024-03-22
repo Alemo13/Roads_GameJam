ad3606a33609ee16c0211941be8dece74d2dc069
Alemo13 <68663794+Alemo13@users.noreply.github.com>
2024-03-21 19:47:31 -0500

    enemy Behaviour

---
+++ Assets/Scripts/AnimationStrings.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+internal class AnimationStrings 
+{
+    internal static string hasTarget = "hasTarget";
+    internal static string isGrounded = "isGrounded";
+    internal static string isOnWall = "isOnWall";
+    internal static string isOnCeiling = "isOnCeiling";
+    internal static string canMove = "canMove";
+}
